64d3854b737a4416018cecc84944be2f978d1c02
Dhruv Makwana <dc_mak@outlook.com>
2017-01-21 19:25:50 +0000

    Update CSV conversion to correct schema

--- dpd_csv.ml
+++ dpd_csv.ml
@@ -16,54 +16,49 @@ let bool_opt_to_bool = function
     | None -> false (* TODO: Warning *)
 
 let print_nodes fmt graph =
-    let print_node n = 
-        let id, name = Node.id n, Node.name n in
-        let body = bool_opt_to_bool (Node.bool_attrib "body" n)
-        and prop = bool_opt_to_bool (Node.bool_attrib "prop" n) in 
-        let kind = let k = str_opt_to_str (Node.get_attrib "kind" n) in 
-                   (* from color assignment documentation *)
-                   let obj_type = function
-                       | true, true -> "lemma"
-                       | true, false -> "definition"
-                       | false, true -> "assumed" (* axiom/admitted *)
-                       | false, false -> "parameter" in
-                   if k = "cnst" then obj_type (body, prop) else k
+  let print_node n = 
+    let id, name = Node.id n, Node.name n in
+    let kind = str_opt_to_str (Node.get_attrib "kind" n) in 
+    let path = str_opt_to_str (Node.get_attrib "path" n) in
+    Format.fprintf fmt "%d,%s,%s,%s@." id name kind path in
 
-        and path = str_opt_to_str (Node.get_attrib "path" n) in
-        Format.fprintf fmt "%d,%s,%s,%s@." id name kind path in
-
-    Format.fprintf fmt "objectId:ID(Object),name,:LABEL,path@.";
-    G.iter_vertex print_node graph
+  (* Header, for Neo4j import *)
+  Format.fprintf fmt "objectId:ID(Object),name,:LABEL,path@.";
+  G.iter_vertex print_node graph
+;;
 
 let print_edges fmt graph =
-    let nb_use e = 
-        try
-            int_of_string (List.assoc "weight" (G.E.label e))
-        with Not_found | Failure _ (* "int_of_string" *) -> 0 in
-    let print_edge e = 
-        Format.fprintf fmt "%d,%d,%d,%s@."
-            (Node.id (G.E.dst e))
-            (Node.id
-            (G.E.src e))
-            (nb_use e)
-            (List.assoc "type" (G.E.label e)) in
-    Format.fprintf fmt ":START_ID(Object),:END_ID(Object),weight,:TYPE@.";
-    G.iter_edges_e print_edge graph
+  let nb_use e = 
+    try int_of_string (List.assoc "weight" (G.E.label e))
+    with Not_found | Failure _ (* "int_of_string" *) -> 0 in
+  let print_edge e = 
+    Format.fprintf fmt "%d,%d,%d,%s@."
+      (Node.id (G.E.dst e))
+      (Node.id
+      (G.E.src e))
+      (nb_use e)
+      (List.assoc "type" (G.E.label e)) in
+
+  (* Header, for Neo4j import *)
+  Format.fprintf fmt ":START_ID(Object),:END_ID(Object),weight,:TYPE@.";
+  G.iter_edges_e print_edge graph
+;;
 
 let try_open_default file_prefix =
-    let filename = file_prefix ^ ".csv" in
-    try open_out filename
-    with Sys_error msg ->
-      C.warning "cannot open file: %s@." msg;
-      let file = Filename.temp_file file_prefix ".dpd" in
-        open_out file
+  let filename = file_prefix ^ ".csv" in
+  try open_out filename
+  with Sys_error msg ->
+    C.warning "cannot open file: %s@." msg;
+    let file = Filename.temp_file file_prefix ".dpd" in
+    open_out file
+;;
 
 let graph_file file_prefix g =
   let node_oc = try_open_default (file_prefix ^ "_node") in
-  let edge_oc = try_open_default (file_prefix ^ "_edge") in
-  C.feedback "Graph output in %s@." (file_prefix ^ "_[node|edge].csv");
   print_nodes (Format.formatter_of_out_channel node_oc) g;
   close_out node_oc;
+  let edge_oc = try_open_default (file_prefix ^ "_edge") in
   print_edges (Format.formatter_of_out_channel edge_oc) g;
-  close_out edge_oc
-
+  close_out edge_oc;
+  C.feedback "Graph output in %s@." (file_prefix ^ "_[node|edge].csv");
+;;
