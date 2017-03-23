de9ef41968ba5201a8a232a01f89a91eb6a37790
Dhruv Makwana <dc_mak@outlook.com>
2017-03-23 13:01:33 +0000

    Add Coq Standard Lib database

--- stdlib_ex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Just a small script to build my example
-set -euo pipefail
-
-echo "Analysing Coq project (seed debug.txt)"
-coqc -R . dpdgraph DpdStdlib.v > debug.txt
-./dpd2csv -keep-trans DpdStdlib.dpd
-./build_graph.sh . DpdStdlib ./coqstdlib
